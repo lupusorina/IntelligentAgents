033c239a8cecd0e0d0d8b6428c5d6b0f542b6f2d
lupusorina <lupusorina@yahoo.com>
2016-09-26 00:04:16 +0200

    Unit Tests added

---
+++ tests/CarryDropModelMoneyGetUT.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CarryDropModelMoneyGetUT {
+	CarryDropModel myObject = new CarryDropModel();
+	@Test
+	public void testGetMoney() {
+		//fail("Not yet implemented");
+		assertEquals(1000, myObject.getMoney());
+	}
+
+}

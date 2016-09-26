0e8f6c358af26d4bf7c0803d78f024dd4cee3140
lupusorina <lupusorina@yahoo.com>
2016-09-26 20:13:34 +0200

    Agent Object constructor created

--- src/CarryDropAgent.java
+++ src/CarryDropAgent.java
@@ -1,5 +1,16 @@
 //package demo;
 
 public class CarryDropAgent {
+	 private int x;
+	 private int y;
+	 private int money;
+	 private int stepsToLive;
 
+	 public CarryDropAgent(int minLifespan, int maxLifespan){
+	    x = -1;
+	    y = -1;
+	    money = 0;
+	    stepsToLive = 
+	        (int)((Math.random() * (maxLifespan - minLifespan)) + minLifespan);
+	  }
 }
